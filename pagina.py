4d11f14bd147657f9a88d6b5d98a319e4483d5f3
coloquio-literatura-queer-peru <coloquio.literatura.queer.pe@gmail.com>
2026-02-16 13:38:33 -0500

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -15,9 +15,43 @@ paginas = ["Presentación", "Calendario", "​Invitados"]
 pagina_seleccionada = st.sidebar.selectbox('Selecciona una sección', paginas)
 
 if pagina_seleccionada == "Presentación":
-    st.markdown("<h1 style='text-align: center; color:#3f8c68; font-size: 80px'>I Coloquio de</h1>", unsafe_allow_html=True)
-    st.markdown("<h1 style='text-align: center; color:#d9307f; font-size: 75px'>Literatura Peruana</h1>", unsafe_allow_html=True)
-    st.markdown("<h1 style='text-align: center; color:#f250c7; font-size: 70px'> Queer</h1>", unsafe_allow_html=True)
+    st.markdown("""
+    <style>
+    .titulo-container {
+        text-align: center;
+        line-height: 1;
+    }
+    
+    .titulo1 {
+        color: #3f8c68;
+        font-size: 80px;
+        margin-bottom: -20px;
+        position: relative;
+        z-index: 1;
+    }
+    
+    .titulo2 {
+        color: #d9307f;
+        font-size: 75px;
+        margin-bottom: -25px;
+        position: relative;
+        z-index: 2;
+    }
+    
+    .titulo3 {
+        color: #f250c7;
+        font-size: 70px;
+        position: relative;
+        z-index: 3;
+    }
+    </style>
+    
+    <div class="titulo-container">
+        <div class="titulo1">I Coloquio de</div>
+        <div class="titulo2">Literatura Peruana</div>
+        <div class="titulo3">Queer</div>
+    </div>
+    """, unsafe_allow_html=True)
     texto_1 = """
     El I Coloquio de Literatura Peruana Queer nace como un espacio de encuentro entre la investigación, la creación literaria y las experiencias artísticas de personas LGBTIQ+, con un énfasis en la literatura producida en el Perú. El coloquio busca visibilizar autorías disidentes, compartir miradas críticas y tejer redes entre escritorxs, investigadorxs y lectorxs, apostando por la construcción de una comunidad literaria más diversa, accesible e inclusiva.
     """
