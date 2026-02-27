5874c6ed4412539db9bc6f568d8135f68d784e50
coloquio-literatura-queer-peru <coloquio.literatura.queer.pe@gmail.com>
2026-02-27 10:27:20 -0500

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -79,8 +79,9 @@ elif pagina_seleccionada == "Equipo organizador":
     with col6:
         st.image("fotos/foto_alan.jpeg", width=260)
         st.markdown("<div style='width:340px; margin:auto; text-align:left; font-size:24px; color:#dc2f82; font-weight:bold;'>Alan Concepción</div>",unsafe_allow_html=True)
-        st.markdown("<div style='width:360px; margin:auto; text-align:left; font-size:18px; color:#3c8d6b; font-weight:bold;'>Coordinación de Producción y Logística</div>",unsafe_allow_html=True)
-        st.markdown("<div style='width:400px; margin:auto; text-align:left; font-size:18px; color:#a154d6;'>alan.concepcion@pucp.pe</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:360px; margin:auto; text-align:left; font-size:18px; color:#3c8d6b; font-weight:bold;'>Coordinación de Producción</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:380px; margin:auto; text-align:left; font-size:18px; color:#3c8d6b; font-weight:bold;'> y Logística</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:380px; margin:auto; text-align:left; font-size:18px; color:#a154d6;'>alan.concepcion@pucp.pe</div>",unsafe_allow_html=True)
 
     st.write("")
     
