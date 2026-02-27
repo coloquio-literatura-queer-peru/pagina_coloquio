5c6a867fbe2928a62043a9fee330a81bca448afc
coloquio-literatura-queer-peru <coloquio.literatura.queer.pe@gmail.com>
2026-02-27 13:37:35 -0500

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -13,18 +13,11 @@ st.set_page_config(
     layout="wide"
 )
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_github_icon = “”"
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 st.markdown("""
     <style>
