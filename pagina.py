024200a58310643c534cc09ea1f72ef45799363a
coloquio-literatura-queer-peru <coloquio.literatura.queer.pe@gmail.com>
2026-02-26 20:29:14 -0500

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -83,7 +83,7 @@ elif pagina_seleccionada == "Sobre nosotrxs":
     
     with col7:
         st.image("fotos/foto_serggio.png", width=260)
-        st.markdown("<div style='width:200px; margin:auto; text-align:left; font-size:24px; color:#dc2f82; font-weight:800;'>Serggio Juarez</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:260px; margin:auto; text-align:left; font-size:24px; color:#dc2f82; font-weight:800;'>Serggio Juarez</div>",unsafe_allow_html=True)
         st.markdown("Coordinación académica")
         st.markdown("serggioart@gmail.com")
         
