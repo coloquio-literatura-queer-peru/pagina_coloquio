dc57803e140de59d31fc53d637b96ef9357558f7
coloquio-literatura-queer-peru <coloquio.literatura.queer.pe@gmail.com>
2026-02-27 10:23:29 -0500

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -72,7 +72,8 @@ elif pagina_seleccionada == "Equipo organizador":
     with col5:
         st.image("fotos/foto_andre.png", width=260)
         st.markdown("<div style='width:380px; margin:auto; text-align:left; font-size:24px; color:#dc2f82; font-weight:bold;'>André Mere Rivera</div>",unsafe_allow_html=True)
-        st.markdown("<div style='width:340px; margin:auto; text-align:left; font-size:18px; color:#3c8d6b; font-weight:bold;'>Coordinación de Comunicaciones y Experiencias del Público</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:340px; margin:auto; text-align:left; font-size:18px; color:#3c8d6b; font-weight:bold;'>Coordinación de Comunicaciones y</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:340px; margin:auto; text-align:left; font-size:18px; color:#3c8d6b; font-weight:bold;'>Experiencias del Público</div>",unsafe_allow_html=True)
         st.markdown("<div style='width:400px; margin:auto; text-align:left; font-size:18px; color:#a154d6;'>andremererivera@gmail.com</div>",unsafe_allow_html=True)
         
     with col6:
