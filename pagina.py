5b9d04261586966c369610e1ccfe7d08f84ca62a
coloquio-literatura-queer-peru <coloquio.literatura.queer.pe@gmail.com>
2026-02-27 14:04:49 -0500

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -149,8 +149,9 @@ elif pagina_seleccionada == "Convocatoria de sumillas":
     </h2>
     """, unsafe_allow_html=True)
     
-    parte_1 = """
-    A pesar del crecimiento de eventos académicos y culturales en el Perú, persisten importantes vacíos en la programación vinculada a las disidencias sexuales y de género, particularmente en el ámbito literario. 
+    st.markdown("""
+    <div style="text-align: justify; font-size:18px;">
+    <p> A pesar del crecimiento de eventos académicos y culturales en el Perú, persisten importantes vacíos en la programación vinculada a las disidencias sexuales y de género, particularmente en el ámbito literario. 
     Las propuestas existentes suelen estar marcadas por enfoques generalistas, sin una atención sostenida a las voces queer ni a las escrituras que tensionan los marcos heteronormativos de representación. 
     Asimismo, la programación cultural tiende a concentrarse en autores y temas ya legitimados, dejando de lado producciones que emergen desde los márgenes, muchas veces autoeditadas, comunitarias o vinculadas a experiencias de precariedad, exclusión y violencia.
     
@@ -159,8 +160,9 @@ elif pagina_seleccionada == "Convocatoria de sumillas":
     
     En ese contexto, el I Coloquio de Literatura Peruana Queer busca atender estas limitaciones mediante una programación que, si bien parte de la investigación académica, la combina con  producciones literarias y experiencias artísticas de sujetos LGBTIQ+ o queer, con un énfasis específico en la literatura peruana, manejando el concepto amplio de “literatura” (que considera las publicaciones independientes, fanzines, novelas gráficas y todo texto “no canónico”). 
     Este primer coloquio crea un espacio interdisciplinario y descentralizado dentro de los circuitos hegemónicos, promoviendo las memorias y reflexiones, la visibilidad de autorías disidentes, la difusión de estudios críticos y la articulación de redes entre creadores/as, investigadores/as y lectores/as. Al hacerlo, contribuye a democratizar el acceso y la representación en el campo literario, desde una mirada situada y plural en el contexto peruano. 
-    """
-    st.markdown(f"<div style='text-align: justify; font-size: 18px;'>{parte_1}<div>", unsafe_allow_html=True)
+    </p>
+    </div>
+    """, unsafe_allow_html=True)
 
     st.markdown("""
     <h2 style="
