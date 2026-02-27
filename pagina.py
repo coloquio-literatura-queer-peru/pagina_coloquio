19e460421103bad35bfd972edbabfff544d5ade5
coloquio-literatura-queer-peru <coloquio.literatura.queer.pe@gmail.com>
2026-02-27 10:33:10 -0500

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -80,7 +80,7 @@ elif pagina_seleccionada == "Equipo organizador":
         st.image("fotos/foto_alan.jpeg", width=260)
         st.markdown("<div style='width:340px; margin:auto; text-align:left; font-size:24px; color:#dc2f82; font-weight:bold;'>Alan Concepción</div>",unsafe_allow_html=True)
         st.markdown("<div style='width:380px; margin:auto; text-align:left; font-size:18px; color:#3c8d6b; font-weight:bold;'>Coordinación de Producción</div>",unsafe_allow_html=True)
-        st.markdown("<div style='width:300px; margin:auto; text-align:left; font-size:18px; color:#3c8d6b; font-weight:bold;'> y Logística</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:280px; margin:auto; text-align:left; font-size:18px; color:#3c8d6b; font-weight:bold;'> y Logística</div>",unsafe_allow_html=True)
         st.markdown("<div style='width:350px; margin:auto; text-align:left; font-size:18px; color:#a154d6;'>alan.concepcion@pucp.pe</div>",unsafe_allow_html=True)
 
     st.write("")
