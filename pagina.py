d0ca8d4b0daa67ab968ae8d38e33643f5b4238fc
coloquio-literatura-queer-peru <coloquio.literatura.queer.pe@gmail.com>
2026-03-29 12:42:47 -0500

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -163,11 +163,11 @@ if pagina_seleccionada == "Presentación":
         </h2>
     """, unsafe_allow_html=True)
     
-    col27, col28 = st.columns(2)
+    col27, col28, col29 = st.columns([1,1,1])
     with col27:
         st.image("fotos/logo_fce.png", width=400)
     with col28:
-        st.image("fotos/logo_gafas_moradas.png", width=350)
+        st.image("fotos/logo_gafas_moradas.png", width=400)
         
 elif pagina_seleccionada == "Equipo organizador":
     st.markdown("""
