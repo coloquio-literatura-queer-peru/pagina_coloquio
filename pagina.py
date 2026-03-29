4c25475fb52676c6f1fafd0ff80f15d2d2c3ee14
coloquio-literatura-queer-peru <coloquio.literatura.queer.pe@gmail.com>
2026-03-28 19:36:21 -0500

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -189,12 +189,11 @@ elif pagina_seleccionada == "Equipo organizador":
         
     # Segunda fila
     col7, col8, col9 = st.columns([1,1,1])
-        
     with col7:
         st.image("fotos/foto_serggio.png", width=400)
-        st.markdown("<div style='width:220px; margin:auto; text-align:center; font-size:24px; color:#dc2f82; font-weight:bold;'>Serggio Juarez</div>",unsafe_allow_html=True)
-        st.markdown("<div style='width:220px; margin:auto; text-align:center; font-size:18px; color:#3c8d6b; font-weight:bold;'>Coordinación Académica</div>",unsafe_allow_html=True)
-        st.markdown("<div style='width:220px; margin:auto; text-align:center; font-size:18px; color:#a154d6;'>serggioart@gmail.com</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:180px; margin:auto; text-align:center; font-size:24px; color:#dc2f82; font-weight:bold;'>Serggio Juarez</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:180px; margin:auto; text-align:center; font-size:18px; color:#3c8d6b; font-weight:bold;'>Coordinación Académica</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:180px; margin:auto; text-align:center; font-size:18px; color:#a154d6;'>serggioart@gmail.com</div>",unsafe_allow_html=True)
            
     with col8:
         st.image("fotos/foto_luisa.png", width=400)
