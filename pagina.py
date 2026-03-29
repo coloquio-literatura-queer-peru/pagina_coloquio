1f3731b3f88c28b1d848de42cf918b8c5d20b21d
coloquio-literatura-queer-peru <coloquio.literatura.queer.pe@gmail.com>
2026-03-28 19:45:56 -0500

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -193,13 +193,13 @@ elif pagina_seleccionada == "Equipo organizador":
         st.image("fotos/foto_serggio.png", width=400)
         st.markdown("<div style='width:180px; margin:auto; text-align:left; font-size:24px; color:#dc2f82; font-weight:bold;'>Serggio Juarez</div>",unsafe_allow_html=True)
         st.markdown("<div style='width:230px; margin:auto; text-align:left; font-size:18px; color:#3c8d6b; font-weight:bold;'>Coordinación Académica</div>",unsafe_allow_html=True)
-        st.markdown("<div style='width:210px; margin:auto; text-align:left; font-size:18px; color:#a154d6;'>serggioart@gmail.com</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:215px; margin:auto; text-align:left; font-size:18px; color:#a154d6;'>serggioart@gmail.com</div>",unsafe_allow_html=True)
            
     with col8:
         st.image("fotos/foto_luisa.png", width=400)
-        st.markdown("<div style='width:200px; margin:auto; text-align:left; font-size:24px; color:#dc2f82; font-weight:bold;'>Luisa Gomez</div>",unsafe_allow_html=True)
-        st.markdown("<div style='width:440px; margin:auto; text-align:left; font-size:18px; color:#3c8d6b; font-weight:bold;'>Coordinación de Difusión y Prensa</div>",unsafe_allow_html=True)
-        st.markdown("<div style='width:380px; margin:auto; text-align:left; font-size:18px; color:#a154d6;'>luisa.gomez@pucp.edu.pe</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:180px; margin:auto; text-align:left; font-size:24px; color:#dc2f82; font-weight:bold;'>Luisa Gomez</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:230px; margin:auto; text-align:left; font-size:18px; color:#3c8d6b; font-weight:bold;'>Coordinación de Difusión y Prensa</div>",unsafe_allow_html=True)
+        st.markdown("<div style='width:200px; margin:auto; text-align:left; font-size:18px; color:#a154d6;'>luisa.gomez@pucp.edu.pe</div>",unsafe_allow_html=True)
     
 elif pagina_seleccionada == "Convocatoria de sumillas":
     st.markdown("""
