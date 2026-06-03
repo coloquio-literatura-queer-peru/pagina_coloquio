b8b509b3ae01e3d3cb86915ef18ceeff40478ace
coloquio-literatura-queer-peru <coloquio.literatura.queer.pe@gmail.com>
2026-06-02 23:58:42 -0500

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -640,48 +640,74 @@ else:
     st.markdown("""
     <div style="
         background-color: #FFC88A;
-        color: #73579d;
         padding: 1rem 1.25rem;
         border-radius: 0.5rem;
         box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 6px;
         font-size: 25px;
         font-weight: bold;
     ">
-        <strong>04:10 PM - 05:20 PM</strong><br> 
+    
+        <strong>04:10 PM - 05:20 PM</strong><br>
+    
         <div style="margin-top:10px;">
-        MESA DE PONENCIAS:
-        <i>Feminidades queer en la literatura peruana contemporánea</i>
+            <span style="color:#dc2982;">
+                MESA DE PONENCIAS:
+            </span>
+            <i>Feminidades queer en la literatura peruana contemporánea</i>
         </div>
+    
         <div style="margin-top:10px;">
-            <b><i>El egoísmo del cuerpo: poéticas del repliegue y disidencia en Matrioska de Valeria Román Marroquín</i></b>
+            <b><i style="color:#3c8e6c;">
+            El egoísmo del cuerpo: poéticas del repliegue y disidencia en Matrioska de Valeria Román Marroquín
+            </i></b>
         </div>
+    
         <div style="margin-top:10px;">
-            Participa: Jimena Sofía Velarde Vera
-        <a href="https://www.instagram.com/velarde_jimena/" target="_blank"
-              style="text-decoration:none; color:#d9307f; font-weight:600;"> (@velarde_jimena)
-        </a>
+            <span style="color:#0a48d5;">Participa: Jimena Sofía Velarde Vera</span>
+    
+            <a href="https://www.instagram.com/velarde_jimena/" target="_blank"
+               style="text-decoration:none; color:#775aa4; font-weight:600;">
+               (@velarde_jimena)
+            </a>
         </div>
+    
         <div style="margin-top:10px;">
-            <b><i>Estética queer y política del cuerpo en la narrativa de Doris Moromisato</i></b>
+            <b><i style="color:#8E24AA;">
+            Estética queer y política del cuerpo en la narrativa de Doris Moromisato
+            </i></b>
         </div>
+    
         <div style="margin-top:10px;">
-            Participa: Judith Mavila Paredes Morales
-        <a href="https://www.instagram.com/judithparedesmorales/" target="_blank"
-              style="text-decoration:none; color:#d9307f; font-weight:600;"> (@judithparedesmorales)
-        </a>
+            <span style="color:#1565C0;">Participa:</span>
+            <span style="color:#424242;"> Judith Mavila Paredes Morales </span>
+    
+            <a href="https://www.instagram.com/judithparedesmorales/" target="_blank"
+               style="text-decoration:none; color:#E1306C; font-weight:600;">
+               (@judithparedesmorales)
+            </a>
         </div>
+    
         <div style="margin-top:10px;">
-            <b><i>«Compórtense como señoritas» de Karen Luy de Aliaga: formación de la subjetividad lésbica entre violencia y deseo</i></b>
+            <b><i style="color:#8E24AA;">
+            «Compórtense como señoritas» de Karen Luy de Aliaga: formación de la subjetividad lésbica entre violencia y deseo
+            </i></b>
         </div>
+    
         <div style="margin-top:10px;">
-            Participa: Karol Nicol Sialas Rosas
-        <a href="https://www.instagram.com/karol_sialas/" target="_blank"
-              style="text-decoration:none; color:#d9307f; font-weight:600;"> (@karol_sialas)
-        </a>
+            <span style="color:#1565C0;">Participa:</span>
+            <span style="color:#424242;"> Karol Nicol Sialas Rosas </span>
+    
+            <a href="https://www.instagram.com/karol_sialas/" target="_blank"
+               style="text-decoration:none; color:#E1306C; font-weight:600;">
+               (@karol_sialas)
+            </a>
         </div>
+    
         <div style="margin-top:10px;">
-            Modera: Camila Arizaca
+            <span style="color:#2E7D32;">Modera:</span>
+            <span style="color:#424242;"> Camila Arizaca </span>
         </div>
+    
     </div>
     """, unsafe_allow_html=True)
     
