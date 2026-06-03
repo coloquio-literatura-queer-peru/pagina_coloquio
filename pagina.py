5f0be96412740581e7cc1e8d4aea5df8fa663be9
coloquio-literatura-queer-peru <coloquio.literatura.queer.pe@gmail.com>
2026-06-02 23:39:12 -0500

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -687,7 +687,7 @@ else:
     
     st.markdown("""
     <div style="
-        background-color: #E57A07;
+        background-color: #6cc19e;
         color: #F4E9E2;
         padding: 1rem 1.25rem;
         border-radius: 0.5rem;
